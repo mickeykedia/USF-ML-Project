dfaef8ba88536d8b0e611da96690e2ba998d8c71
Jaime Pastor <jaime@Jaimes-MacBook-Pro.local>
2015-12-03 18:37:15 -0800

    snippets data description

---
+++ ML_FP_Data_description.R
@@ -0,0 +1,64 @@
+# MSAN621 - Machine Learning
+# Final Project: Classification
+
+library(corrplot)
+
+# (0) DATA DESCRIPTION #######################################################
+Y = df[,2]
+X = df[,-2]
+data.description <- function(X, Y = null){
+  
+  # Check if Y is first column of X, and bind in single dataframe
+  if (!is.null(Y)){
+    df = cbind(Y,X) 
+  }else{
+    df = X
+  }
+  
+  # Print n, p
+  n = now(df)
+  p = ncol(df) - 1
+  cat('\n##### DATA DESCRIPTION #####',
+    '\nNumber of observations: ', n, '\nNumber of variables: ',p)
+  
+  # Number of NA values
+  cat('\nNumber of missing values: ', sum(is.na(df)))
+  cat('\nNumber of complete observations: ', sum(complete.cases(df)))
+  
+  # Type of variables
+  s = lapply(df, class)
+  cat('Number of continuous variables:', sum(s == "numeric") + sum(s == "integer"))
+  cat('Number of categorical variables:', sum(s == "factor"))
+  cat('Number of binary variables:', sum(lapply(lapply(df, unique),length) == 2))
+  
+  # Number of significant variables in logistic regression
+  X2 = data.matrix(df[,2:(p+1)])
+  Y2 = data.matrix(df[,1,drop=FALSE])
+  glm.fit <- glm(Y2 ~ X2, family = binomial)
+  s = summary(glm.fit)
+  p_vals = s$coefficients[-1,4]
+  cat('Number of statistically significant variables (in logistic regression):',
+      sum(p_vals < 0.05))
+  
+  # Comparison of means and sds
+  numeric_cols = sapply(df, is.numeric)
+  df_numeric = df[,numeric_cols]
+  var_mean = lapply(df_numeric, mean, na.rm = TRUE)
+  var_sd = lapply(df_numeric, sd, na.rm = TRUE)
+  df_msd = data.frame('Mean' = unlist(var_mean), 
+                      'SD' = unlist(var_sd))
+  cat('\nMean and standard deviation of numeric variables:')
+  print.data.frame(df_msd, digits = 3, right = FALSE)
+  
+  # Check potentially colinear variables
+  par(mfrow = c(1,1))
+  df_cor = data.frame(cor(na.omit(df_numeric[,2:ncol(df_numeric)])))
+  #corrplot.mixed(cor(na.omit(df_numeric[,2:ncol(df_numeric)])), 
+  #      upper='circle', tl.col='black')
+  
+  
+} 
+
+
+
+
