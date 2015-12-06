954a88a0ed9622577e3475171f582c02df0304b5
swetha <svenkatareddy@dons.usfca.edu>
2015-12-05 20:31:30 -0800

    MASS package LDA added

--- ML_lda.R
+++ ML_lda.R
@@ -2,7 +2,7 @@
 library(caret)
 
 ####################Linear Discriminant Analysis################
-LDA = function(X_train, Y_train, X_test, Y_test){
+LDA_caret = function(X_train, Y_train, X_test, Y_test){
   trCtl=trainControl(method='repeatedcv',number=4)
   model = train(X_train, Y_train,'lda',trControl=trCtl)
   pred <- predict(model$finalModel, newdata = as.data.frame(X_test))
@@ -11,6 +11,47 @@ LDA = function(X_train, Y_train, X_test, Y_test){
   return(model, pr)
 }
 
+checkNormality  =function(tarin){
+  n <- ncol(train)
+  str <- vector()
+  for(i in 2:n){
+    predictor <- train[,i]
+    sh.out <- shapiro.test(predictor)
+    if (sh.out$p.value > 0.05){
+      outputStr <- paste0(names(train)[i], " is normally distributed.")  
+    } else{
+      outputStr <- paste0(names(train)[i], " is not normally distributed.")
+    }
+    str[i-1] <- outputStr
+  }
+  return(str)
+}
+
+
+LDA = function(train, test){
+  #####Check Normality
+  outnormal <- checkNormality(train)
+  df <- data.frame(train)
+  # change the name of response to Y
+  names(df)[1]<-c('Y')
+  coln = colnames(df)
+  lda.fit <- lda(Y~., data = df, na.omit =TRUE)
+  
+  dfTest <- data.frame(test)
+  names(dfTest)[1]<-c('Y')
+  lda.pred <- predict(lda.fit, dfTest$Y)
+  names(lda.pred)
+  
+  #evaluate how the method performed
+  lda.class <- lda.pred$class
+  confusion <- table(lda.class, dfTest$Y)
+  #model the accuracy
+  accuracy <- mean(lda.class == dfTest$Y)
+  
+  #the posterior probability
+  posteriorProb <- lda.pred$posterior
+  return(lda.fit, lda.pred, accuracy, confusion)
+}
 
 
 #######NAIVE BAYES#############
